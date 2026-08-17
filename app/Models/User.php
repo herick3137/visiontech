6a1d2a80d821c13408e6333f42b09e70a76d9f54
Herick Filipe Klann Lima <klannlimaherickfilipe@gmail.com>
2026-08-17 19:56:03 -0300

    Enhance User model with FilamentUser interface
    Implemented FilamentUser interface and access control method.

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,7 +4,9 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Database\Factories\UserFactory;
+use Filament\Models\Contracts\FilamentUser;
 use Filament\Models\Contracts\HasName;
+use Filament\Panel;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -13,10 +15,18 @@ use Illuminate\Notifications\Notifiable;
 
 #[Fillable(['name', 'email', 'password', 'cargo', 'perfil'])]
 #[Hidden(['password', 'remember_token'])]
-class User extends Authenticatable implements HasName
+class User extends Authenticatable implements HasName, FilamentUser
 {
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
+
+    /**
+     * Libera o acesso ao painel do Filament para todos os usuários cadastrados.
+     */
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return true; // Retorna "true" permitindo que todos com conta acessem
+    }
 
     /**
      * Exibe o Nome e o Cargo concatenados no cabeçalho do Filament.
