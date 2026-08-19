84f2a17b4344ec48baa4dfdcf34b4c93ed68d565
Herick Filipe Klann Lima <klannlimaherickfilipe@gmail.com>
2026-08-19 16:50:02 -0300

    Remove admin logout route and add home route
    Removed admin logout route and added home route.

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,7 @@
 <?php
+
 use Illuminate\Support\Facades\Route;
-use Filament\Facades\Filament;
 
-Route::post('/admin/logout', function () {
-    Filament::auth()->logout();
-
-    request()->session()->invalidate();
-    request()->session()->regenerateToken();
-
-    return redirect('/admin/login');
-})->name('admin.logout');
+Route::get('/', function () {
+    return view('welcome');
+});
